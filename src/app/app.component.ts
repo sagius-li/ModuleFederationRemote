fb910c7c501d80500ec8251062fb5b0c92101039
Jie Li <jl@ocg.de>
2026-04-29 16:51:26 +0200

    added angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { MatButtonModule } from "@angular/material/button";
+
 @Component({
   selector: 'app-root',
-  imports: [RouterModule],
+  imports: [RouterModule, MatButtonModule],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,10 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
 import { REMOTE_CHILD_ROUTES } from './app/remote-child.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(REMOTE_CHILD_ROUTES)]
+  providers: [provideRouter(REMOTE_CHILD_ROUTES), provideAnimations()]
 })
   .catch(err => console.error(err));
